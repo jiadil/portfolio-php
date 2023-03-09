ebc0f34849c253916972eba9a297d88ff88d4574
jiadil <jiadil@sfu.ca>
2023-03-08 20:38:41 -0800

    create new files for sections

---
+++ src/content/section-qualifications.php
@@ -0,0 +1,92 @@
+<section class="qualification section">
+    <h2 class="section__title" i18n="qualification__title">Qualification</h2>
+    <span class="section__subtitle" i18n="qualification__subtitle">My personal journey</span>
+    <div class="qualification__container container">
+        <div class="qualification__tabs">
+            <div class="qualification__button button--flex qualification__active" data-target="#education">
+                <i class="uil uil-graduation-cap qualification__icon"></i>
+                <span i18n="education">Education</span>
+            </div>
+
+            <div class="qualification__button button--flex" data-target="#work">
+                <i class="uil uil-briefcase-alt qualification__icon"></i>
+                <span i18n="work">Work</span>
+            </div>
+        </div>
+
+        <div class="qualification__sections">
+            <!--==================== QUALIFICATION CONTENT 1 ====================-->
+            <div class="qualification__content qualification__active" data-content id="education">
+                <!--==================== QUALIFICATION 1 ====================-->
+                <div class="qualification__data">
+                    <div>
+                        <h3 class="qualification__title" i18n="qualification1__title">Computer Enginner</h3>
+                        <span class="qualification__subtitle" i18n="qualification1__subtitle">**** - University</span>
+                        <div class="qualification__calendar">
+                            <i class="uil uil-calendar-alt"></i>
+                            2019 - 2023
+                        </div>
+                    </div>
+
+                    <div>
+                        <span class="qualification__rounder"></span>
+                        <span class="qualification__line"></span>
+                    </div>
+                </div>
+                <!--==================== QUALIFICATION 2 ====================-->
+                <div class="qualification__data">
+                    <div></div>
+                    <div>
+                        <span class="qualification__rounder"></span>
+                        <span class="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 class="qualification__title" i18n="qualification2__title">Web Design</h3>
+                        <span class="qualification__subtitle" i18n="qualification2__subtitle">Self-Study</span>
+                        <div class="qualification__calendar">
+                            <i class="uil uil-calendar-alt"></i>
+                            2022.12-2023
+                        </div>
+                    </div>
+                </div>
+                <!--==================== QUALIFICATION 3 ====================-->
+                <div class="qualification__data">
+                    <div>
+                        <h3 class="qualification__title" i18n="qualification3__title">Web Development</h3>
+                        <span class="qualification__subtitle" i18n="qualification3__subtitle">Self-Study</span>
+                        <div class="qualification__calendar">
+                            <i class="uil uil-calendar-alt"></i>
+                            2023 - ...
+                        </div>
+                    </div>
+
+                    <div>
+                        <span class="qualification__rounder"></span>
+                        <!-- <span class="qualification__line"></span> -->
+                    </div>
+                </div>
+            </div>
+
+            <!--==================== QUALIFICATION CONTENT 2 ====================-->
+            <div class="qualification__content" data-content id="work">
+                <!--==================== QUALIFICATION 1 ====================-->
+                <div class="qualification__data">
+                    <div></div>
+                    <div>
+                        <span class="qualification__rounder"></span>
+                        <!-- <span class="qualification__line"></span> -->
+                    </div>
+                    <div>
+                        <h3 class="qualification__title" i18n="qualification4__title">Software Enginner</h3>
+                        <span class="qualification__subtitle" i18n="qualification4__subtitle">ShenZhen</span>
+                        <div class="qualification__calendar">
+                            <i class="uil uil-calendar-alt"></i>
+                            2022
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
